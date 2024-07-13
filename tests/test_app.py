0403d0e0d3767717665acf5422ef1f12448f4de9
Kasor Sensei <cbdiesse@gmail.com>
2024-07-13 17:54:22 -0400

    commit 29th

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,6 @@ from kaspy import app
 def test_index():
     client = app.create_app()
     response = client.get('/')
+    html = response.data.decode()
+    assert 'Hello, everyone ! ... hello Docker!' in html
     assert response.status_code == 200
