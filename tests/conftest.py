442d6830249083112cf88ac03a8fcd439c9a3281
Kasor Sensei <cbdiesse@gmail.com>
2024-07-13 17:42:45 -0400

    commit 28th

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,20 @@ from kaspy.app import create_app
 @pytest.fixture(scope='session')
 def test_client():
     test_app = create_app()
+    testing_client = test_app.test_client()
+    ctx = test_app.app_context()
+    ctx.push()
     test_app.config.update({
         "TESTING": True,
     })
 
     # other setup can go here
 
-    yield test_app
+    yield test_client
 
     # clean up / reset resources here
-
+    ctx.pop()
+    
 
 @pytest.fixture()
 def client(app):
